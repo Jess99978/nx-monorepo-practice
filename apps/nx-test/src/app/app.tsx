4739a46a5b84787462db9bd9426242c50612a593
Jess <candylee022365@gmail.com>
2024-09-16 15:18:24 +0800

    feat: create new React component "my-component" using Nx Console

--- apps/nx-test/src/app/app.tsx
+++ apps/nx-test/src/app/app.tsx
@@ -1,9 +1,11 @@
+import '../styles.css';
 import NxWelcome from './nx-welcome';
-
+import MyComponent from '../components/my-component';
 export function App() {
   return (
     <div>
       <NxWelcome title="nx-test" />
+      <MyComponent />
     </div>
   );
 }
---
+++ apps/nx-test/src/components/my-component.tsx
@@ -0,0 +1,13 @@
+export function MyComponent() {
+  return (
+    <>
+      <h1>Welcome to MyComponent!</h1>
+      <h2 className="tlw-text-xl tlw-text-red-300">Hello world!</h2>
+      <div className="tlw-m-2 tlw-h-10 tlw-w-screen tlw-pl-4 tlw-bg-blue-200 tlw-pr-3">
+        123
+      </div>
+    </>
+  );
+}
+
+export default MyComponent;
