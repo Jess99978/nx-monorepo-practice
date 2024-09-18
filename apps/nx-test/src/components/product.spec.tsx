7016ab62164918ce5dd53555db8e55731264bb6f
Jess <candylee022365@gmail.com>
2024-09-18 18:02:21 +0800

    feat(nx-test): finish "Product" component

--- apps/nx-test/src/components/product.spec.tsx
+++ apps/nx-test/src/components/product.spec.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 
-import Product from './product';
+import Product from './Product';
 
 describe('Product', () => {
   it('should render successfully', () => {
--- apps/nx-test/src/components/product.tsx
+++ apps/nx-test/src/components/product.tsx
@@ -1,19 +1,34 @@
-export default function Product() {
-  return (
-    <div className="tlw-w-48 tlw-border tlw-border-gray-200 tlw-flex tlw-flex-col tlw-gap-3 tlw-px-1 hover:tlw-cursor-pointer">
-      <img src="../assets/product-01.jpg" alt="" />
-      <p className="tlw-text-sm">3M 3US-F011-5 活性碳替換濾心</p>
-      <div className="tlw-flex tlw-items-center tlw-justify-between">
-        <div className="tlw-flex tlw-items-center tlw-gap-2">
-          <p className="tlw-text-primary tlw-text-lg tlw-font-semibold">
-            $2,780
-          </p>
-          <p className="tlw-text-sm tlw-text-gray-500 tlw-line-through">$2,880</p>
+interface productProps {
+    product: {
+      id: number;
+      name: string;
+      discountPrice: number;
+      price: number;
+      image: string;
+    };
+  }
+  
+  export default function Product({ product }: productProps) {
+    return (
+      <div className="tlw-flex tlw-justify-between tlw-gap-3 tlw-border tlw-border-gray-200 tlw-px-1 hover:tlw-cursor-pointer sm:tlw-flex-col">
+        <img src={`../assets/${product.image}`} alt={product.name} />
+        <div className="tlw-flex tlw-w-56 tlw-flex-col tlw-justify-center tlw-gap-4 sm:tlw-w-auto sm:tlw-gap-0 tlw-items-center  sm:tlw-items-start">
+          <p className="tlw-text-md sm:tlw-text-sm">{product.name}</p>
+          <div className="tlw-flex tlw-flex-col tlw-items-center tlw-gap-3 sm:tlw-flex-row sm:tlw-justify-between sm:tlw-gap-0">
+            <div className="tlw-flex tlw-items-center tlw-gap-2">
+              <p className="tlw-text-lg tlw-font-semibold tlw-text-primary">
+                {product.discountPrice}
+              </p>
+              <p className="tlw-text-sm tlw-text-gray-500 tlw-line-through">
+                {product.price}
+              </p>
+            </div>
+            <button>
+              <img src="../assets/cart.svg" alt="" />
+            </button>
+          </div>
         </div>
-        <span>
-          <img src="../assets/cart.svg" alt="" />
-        </span>
       </div>
-    </div>
-  );
-}
+    );
+  }
+  
