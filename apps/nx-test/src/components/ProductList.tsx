c644312c5fa1e3fdd7799faa5dc437112496c35e
Jess <candylee022365@gmail.com>
2024-09-19 22:41:14 +0800

    style(nx-test/ProductList): add whitespace on title

--- apps/nx-test/src/components/ProductList.tsx
+++ apps/nx-test/src/components/ProductList.tsx
@@ -4,7 +4,7 @@ import productData from '../mock-data/products.json';
 export default function ProductList() {
   return (
       <div className="tlw-mt-5 tlw-mx-5">
-        <h1 className="tlw-text-3xl tlw-font-bold tlw-mb-5"><span className='tlw-text-3xl tlw-font-bold tlw-text-primary'>特力屋</span>商品列表頁</h1>
+        <h1 className="tlw-text-3xl tlw-font-bold tlw-mb-5"><span className='tlw-text-3xl tlw-font-bold tlw-text-primary'>特力屋</span>&ensp;商品列表頁</h1>
         <div className="tlw-flex tlw-max-w-7xl tlw-flex-col tlw-gap-3 sm:tlw-flex-row">
           {productData.map((product) => {
             return <Product key={product.id} product={product} />;
