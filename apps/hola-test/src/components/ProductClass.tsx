c148229f325559ea044454c3ac0cf5d5f532bee4
Jess <candylee022365@gmail.com>
2024-09-23 23:02:11 +0800

    feat(hola-test): add state and methods to manage "ProductClass" component

--- apps/hola-test/src/components/ProductClass.tsx
+++ apps/hola-test/src/components/ProductClass.tsx
@@ -1,11 +1,18 @@
 import { Component } from 'react';
+import clsx from 'clsx';
 
-// 定義傳入狀態的型別 
+// 定義傳入狀態的型別
 interface ProductState {
   inCart: boolean;
+  stock: number;
+  disabled:boolean
 }
 // 定義傳入值的型別（若沒有接收 props 就定義為空）
-interface ProductProps {}
+interface ProductProps {
+  name: string;
+  price: number;
+  image: string;
+}
 
 // Component 接收兩個參數：組件的 props 類型以及 state 類型
 class ProductClass extends Component<ProductProps, ProductState> {
@@ -16,29 +23,57 @@ class ProductClass extends Component<ProductProps, ProductState> {
     // 初始化 state
     this.state = {
       inCart: false,
+      stock: 8,
+      disabled:false
     };
   }
-  addToCart = () => {
-    this.setState({ inCart: true });
-  };
-//   一定要使用 render 方法來渲染
-  render() {
-    //  解構出 inCart 狀態
-    const { inCart } = this.state;
+  //   addToCart = () => {
+  //     this.setState({ inCart: true });
+  //   };
 
+  // 減少庫存的方法(點擊按鈕觸發)
+  reduceStock = () => {
+    this.setState((prevState) => ({
+      stock: prevState.stock - 1,
+    }));
+  };
+
+  // ComponentDidUpdate 接收兩個參數（先前參數、先前狀態）:在狀態更新時觸發
+  componentDidUpdate(
+    prevProps: Readonly<ProductProps>,
+    prevState: Readonly<ProductState>,
+    snapshot?: any,
+  ): void {
+    // 自動更新庫存數量
+    // if (prevState.stock !== this.state.stock && this.state.stock < 5) {
+    // //   this.setState({ stock: 20 });
+    // }
+    if (prevState.stock !== this.state.stock && this.state.stock === 0) {
+      this.setState({disabled:true});
+    }
+  }
+
+  //   一定要使用 render 方法來渲染
+  render() {
+    //  解構出 inCart 和 stock 狀態
+    const { stock,disabled } = this.state;
     return (
       <div className="hola-flex-col hola-gap-4 hola-flex">
-        <img src="../../assets/sofa-01.jpg" alt="sofa" />
-        <h1 className="hola-text-lg hola-text-">
-          FLEXLUX Torino 布質沙發右L 217cm 粉藕
-        </h1>
+        <img src={`../../assets/${this.props.image}`} alt="sofa" />
+        <h1 className="hola-text-lg hola-text-">{this.props.name}</h1>
         <div className="hola-flex-col hola-flex hola-gap-3">
-          <p>$89900</p>
+          <div className="hola-flex hola-justify-between">
+            <p>{this.props.price}</p>
+            <p>庫存:{stock}</p>
+          </div>
           <button
-            className="hola-p-3 hola-text-gray-200 hola-bg-black hover:hola-bg-primary hover:hola-text-black"
-            onClick={this.addToCart}
+            className={clsx(disabled?'hola-p-3 hola-text-gray-700 hola-bg-gray-200':
+              'hola-p-3 hola-text-gray-200 hola-bg-black hover:hola-bg-primary hover:hola-text-black'
+            )}
+            onClick={this.reduceStock}
+            disabled={disabled}
           >
-            {inCart ? '已選購' : '立即選購'}
+            立即選購
           </button>
         </div>
       </div>
