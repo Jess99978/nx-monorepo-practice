a685e15b7246c0750e232aa5e1b28f965f32ad1f
Jess <candylee022365@gmail.com>
2024-09-24 14:04:21 +0800

    feat(hola-test): create "Countdown" component using class syntax

--- apps/hola-test/src/app/app.tsx
+++ apps/hola-test/src/app/app.tsx
@@ -1,10 +1,12 @@
 import '../styles.css';
 import Header from '../components/Header';
 import ProductList from '../components/ProductList';
+import Countdown from '../components/Countdown';
 export function App() {
   return (
     <>
       <Header />
+      <Countdown/>
       <ProductList/>
     </>
   );
