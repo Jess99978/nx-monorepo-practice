52274ef4dcae3ef2dbd9af1848b782f24e780a20
Jess <candylee022365@gmail.com>
2024-09-27 14:02:50 +0800

    feat(hola-test): set up Redux and use it in "Counter" class component

--- apps/hola-test/src/app/app.tsx
+++ apps/hola-test/src/app/app.tsx
@@ -2,12 +2,15 @@ import '../styles.css';
 import Header from '../components/Header';
 import ProductList from '../components/ProductList';
 import Countdown from '../components/Countdown';
+import Counter from '../components/Counter';
+
 export function App() {
   return (
     <>
-      <Header />
-      <Countdown/>
-      <ProductList/>
+      {/* <Header /> */}
+      {/* <Countdown/> */}
+      <Counter/>
+      {/* <ProductList/> */}
     </>
   );
 }
--- apps/hola-test/src/main.tsx
+++ apps/hola-test/src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-
 import App from './app/app';
+import { Provider } from 'react-redux';
+import store from './app/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <StrictMode>
-    <App />
+    {/* Provider 使用 react 的 context API 共享全域資料 */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
