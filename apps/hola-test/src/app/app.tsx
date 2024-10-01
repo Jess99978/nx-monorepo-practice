973b67878740450b8de9bf91d871976e0c2c8a69
Jess <candylee022365@gmail.com>
2024-10-01 13:56:07 +0800

    chore(hola-test): adjust layout to demo new Zustand feature

--- apps/hola-test/src/app/app.tsx
+++ apps/hola-test/src/app/app.tsx
@@ -3,14 +3,17 @@ import Header from '../components/Header';
 import ProductList from '../components/ProductList';
 import Countdown from '../components/Countdown';
 import Counter from '../components/Counter';
-
+import CartList from '../components/CartList';
 export function App() {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       {/* <Countdown/> */}
-      <Counter/>
-      {/* <ProductList/> */}
+      {/* <Counter/> */}
+      <div className='hola-flex hola-gap-2'>
+      <ProductList/>
+      <CartList/>
+      </div>
     </>
   );
 }
--- apps/hola-test/src/components/ProductList.tsx
+++ apps/hola-test/src/components/ProductList.tsx
@@ -5,14 +5,15 @@ import data from '../mock-data/products.json';
 export default function ProductList() {
   return (
     <>
-      <h1 className="hola-m-3 hola-text-3xl">functional component</h1>
-      <div className="hola-max-w-7xl hola-px-5 hola-grid hola-grid-cols-2 hola-grid-rows-2 lg:hola-grid-cols-4 lg:hola-grid-rows-1 hola-gap-7">
+      {/* <h1 className="hola-m-3 hola-text-3xl">functional component</h1> */}
+      <div className="hola-max-w-7xl hola-px-5 hola-grid hola-grid-cols-2 hola-grid-rows-2 lg:hola-grid-cols-4 lg:hola-grid-rows-1 hola-gap-7 hola-mb-10">
         {data.map((product, index) => {
           return <Product key={index} product={product} />;
         })}
       </div>
-      <h1 className="hola-m-3 hola-text-3xl">class component</h1>
-      <div className="hola-max-w-7xl hola-px-5 hola-grid hola-grid-cols-2 hola-grid-rows-2 lg:hola-grid-cols-4 lg:hola-grid-rows-1 hola-gap-7 hola-mb-5">
+      <hr />
+        {/* <h1 className="hola-m-3 hola-text-3xl">class component</h1> */}
+        {/* <div className="hola-max-w-7xl hola-px-5 hola-grid hola-grid-cols-2 hola-grid-rows-2 lg:hola-grid-cols-4 lg:hola-grid-rows-1 hola-gap-7 hola-mb-5">
         {data.map((product, index) => {
           return (
             <ProductClass
@@ -23,7 +24,7 @@ export default function ProductList() {
             />
           );
         })}
-      </div>
+      </div> */}
     </>
   );
 }
