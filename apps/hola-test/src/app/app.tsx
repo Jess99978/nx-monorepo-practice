b65774538c2aa2eb9d06444144e7215cbc47aad1
Jess <candylee022365@gmail.com>
2024-10-04 11:37:16 +0800

    chore(hola-test): adjust layout to demo component "Location" feature

--- apps/hola-test/src/app/app.tsx
+++ apps/hola-test/src/app/app.tsx
@@ -1,19 +1,9 @@
 import '../styles.css';
-import Header from '../components/Header';
-import ProductList from '../components/ProductList';
-import Countdown from '../components/Countdown';
-import Counter from '../components/Counter';
-import CartList from '../components/CartList';
+import Location from '../components/Location';
 export function App() {
   return (
     <>
-      <Header />
-      {/* <Countdown/> */}
-      {/* <Counter/> */}
-      <div className='hola-flex hola-gap-2'>
-      <ProductList/>
-      <CartList/>
-      </div>
+      <Location/>
     </>
   );
 }
